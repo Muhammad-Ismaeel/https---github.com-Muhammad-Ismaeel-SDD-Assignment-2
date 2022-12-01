3cb40dc9a6d5d5632cd0e72663c146a6ddd0f0de
Muhammad-Ismaeel <s10219144@connect.np.edu.sg>
2022-12-01 11:42:18 +0800

    started on mainmenu function

--- Assignment-2-Code.py
+++ Assignment-2-Code.py
@@ -1,2 +1,11 @@
-print('This is our assignment for SDD')
-print('Hello World!')
+## SDD ASSIGNMENT 2 ##
+
+def mainmenu():  # MAIN MENU FUNCTION ##
+    print('1. Start new game')
+    print('2. Load game')
+    print('3. Show high scores')
+    print()
+    print('0. Exit')
+
+
+mainmenu()
