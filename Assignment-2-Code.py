f07a2ccd2815d4d74274155d47e1ffcb5fd71b53
Muhammad-Ismaeel <s10219144@connect.np.edu.sg>
2022-12-01 12:52:01 +0800

    loop for running game created

--- Assignment-2-Code.py
+++ Assignment-2-Code.py
@@ -6,6 +6,19 @@ def mainmenu():  # MAIN MENU FUNCTION ##
     print('3. Show high scores')
     print()
     print('0. Exit')
+    print()
 
 
-mainmenu()
+# while loop to run the game
+i = 1
+while (i > 0):
+    mainmenu()
+    choice = int(input("What is your choice? "))
+    if choice == 1:
+        print('Game started!')
+    elif choice == 2:
+        print("Game Loaded!")
+    elif choice == 3:
+        print("Here are your high scores!")
+    else:
+        break
